c207997a2e2ad1616e0c549e5b1fa29e2f6e4657
laurasturaine <125255157+laurasturaine@users.noreply.github.com>
2023-03-19 19:56:54 +0000

    update2

--- main.py
+++ main.py
@@ -1,45 +1,29 @@
 # python3
-import heapq
+
 
 def parallel_processing(n, m, data):
-    threads=[(i,0) for i in range(n)]
-    heapq.heapify(threads)
-
-    jobs[(i,data[i]) for i in range(m)]
+    reizes1=[0]*n
     output = []
 
-    for job in jobs:
-        thread = heapq.heappop(threads)
-        output.append((thread[0], thread[1]))
-        heapq.heappush(threads, (thread[0], thread[1] + job[1]))
+    for x in range(m):
+        laiks_min=min(reizes1)
+        index=reizes1.index(laiks_min)
+        output.append((reizes1,laiks_min))
+        if x<len(data):
+            reizes1[index]+=data[x]
 
-    # TODO: write the function for simulating parallel tasks, 
-    # create the output pairs
 
     return output
 
 def main():
-    # TODO: create input from keyboard
-    # input consists of two lines
-    # first line - n and m
-    # n - thread count 
-    # m - job count
-    n = 0
-    m = 0
+
     n,m=map(int,input().split())
-    data=list(mp(int,input().split()))
-    output=parallel_processing(n,m,data)
-
-    # second line - data 
-    # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
-    
-
-    # TODO: create the function
+    data=list(map(int,input().split()))
     result = parallel_processing(n,m,data)
     
     # TODO: print out the results, each pair in it's own line
-    for r in output:
-        print(r[0], r[1])
+    for thread_index, sak_laiks in result:
+        print(thread_index, sak_laiks)
 
 
 if __name__ == "__main__":
